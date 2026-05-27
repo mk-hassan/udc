72cc1ed4a315afd09962e424617cc252b8ec4cbb
Mahmoud Kamal <mk.bayumi@gmail.com>
2026-05-27 03:35:08 +0300

    feat(constants): add constants for file arguments and block sizes

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+pub const INPUT_FILE_ARG: &str = "if";
+pub const OUTPUT_FILE_ARG: &str = "of";
+pub const BLOCK_SIZE_ARG: &str = "bs";
+pub const INPUT_BLOCK_SIZE_ARG: &str = "ibs";
+pub const OUTPUT_BLOCK_SIZE_ARG: &str = "obs";
+
+pub const DEFAULT_BLOCK_SIZE: usize = 512;
+pub const MAX_BLOCK_SIZE: usize = 1024 * 1024 * 1024; // 1GB
